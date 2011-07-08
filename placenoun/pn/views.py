7a8e634605900d2d033d08681dd2c10a03e53601
Aaron Merriam <amerriam@fusionbox.com>
2011-07-08 10:42:06 -0600

    Trying to fix some more bugs

--- placenoun/pn/views.py
+++ placenoun/pn/views.py
@@ -19,12 +19,12 @@ def noun_static(request, noun, width, height):
   height = min(2048, int(height))
 
   noun_query = NounStatic.objects.filter(noun = noun, width = width, height = height)[:1]
-  if noun_query.exists():
+  if noun_query:
     this_image = noun_query.get()
     return this_image.http_image
 
   noun_query = NounExternal.objects.filter(available = True, noun = noun, width = width, height = height)[:1]
-  if noun_query.exists():
+  if noun_query:
     this_image = noun_query.get()
     if this_image.id:
       this_image = this_image.to_static()
@@ -35,7 +35,7 @@ def noun_static(request, noun, width, height):
   aspect = Decimal(width)/Decimal(height)
   noun_query = NounExternal.objects.filter(available = True, noun= noun, aspect = aspect, width__gte = width, height__gte = height)[:1]
   q2 = noun_query
-  if noun_query.exists():
+  if noun_query:
     this_image = noun_query.get()
     if this_image.id:
       this_image = this_image.to_static(size=(width, height))
@@ -52,7 +52,7 @@ def noun_static(request, noun, width, height):
     noun_query = NounExternal.objects.filter(noun = noun).filter(
       width__lte = width + radius, height__lte = height + radius).filter(
       width__gte = width - radius, height__gte = height - radius)[:100]
-    if not noun_query.exists():
+    if not noun_query:
       radius = radius*2
       if radius > 2000:
         random.choice([SearchBing, SearchGoogle]).do_next_search(noun)
@@ -65,9 +65,10 @@ def noun_static(request, noun, width, height):
     return this_image.http_image_resized(size=(width, height))
 
 def noun(request, noun):
-  noun_query = NounExternal.objects.filter(noun = noun).order_by('?')[:1]
-  if noun_query.exists():
+  noun_query = NounExternal.objects.filter(noun = noun).order_by('?')[:100]
+  if noun_query:
     if noun_query.count() > 100:
+      noun_query = noun_query[:1]
       this_image = noun_query.get()
       if this_image.id:
         return this_image.http_image
@@ -76,7 +77,7 @@ def noun(request, noun):
 
   while True:
     noun_query = NounExternal.objects.filter(noun = noun).order_by('?')[:1]
-    if noun_query.exists():
+    if noun_query:
       this_image = noun_query.get()
       if not this_image.id:
         continue
